0396fcf1cea3d8f29a98dc45f32012ed571e2cc1
Piotr Sekula <Piotr_Sekula@epam.com>
2017-07-04 08:53:23 +0200

    Add match engine to allow user play 3 games

--- src/main/java/com/gawdski/tictactoe/App.java
+++ src/main/java/com/gawdski/tictactoe/App.java
@@ -4,23 +4,8 @@ public class App {
 
     public static void main(String[] args) {
 
-        Communicable messanger = InitializationEngine.selectLangClass();
-        Board board = new Board(messanger.askForBoardSize(2, 1000));
-        Players players = new Players();
-        players.initializePlayers(
-                messanger.askForStartingSymbol(),
-                messanger.askPlayer1ForName(),
-                messanger.askPlayer2ForName());
-
-        GameEngine gameEngine = new GameEngine(board, players);
-        int moveId = 0;
-        board.printBoard();
-
-        while (!gameEngine.isGameFinished()) {
-            gameEngine.makeMove(board, moveId, messanger);
-            moveId++;
-        }
-        gameEngine.updatePoints();
-        gameEngine.displayCurrentResult(messanger);
+        MatchEngine matchEngine = new MatchEngine();
+        matchEngine.initializeMatch();
+        matchEngine.playMatch();
     }
 }
---
+++ src/main/java/com/gawdski/tictactoe/MatchEngine.java
@@ -0,0 +1,41 @@
+package com.gawdski.tictactoe;
+
+class MatchEngine {
+
+    private final int noOfGames = 3;
+
+    private Communicable messanger;
+    private GameEngine gameEngine;
+    private int boardSize;
+    private Players players;
+
+    void initializeMatch() {
+
+        messanger = InitializationEngine.selectLangClass();
+        boardSize = messanger.askForBoardSize(2, 1000);
+        players = new Players();
+        players.initializePlayers(
+                messanger.askForStartingSymbol(),
+                messanger.askPlayer1ForName(),
+                messanger.askPlayer2ForName());
+    }
+
+    void playMatch() {
+
+        int gameId = 0;
+        while (gameId < noOfGames) {
+            Board board = new Board(boardSize);
+            gameEngine = new GameEngine(board, players);
+            int moveId = 0;
+            board.printBoard();
+
+            while (!gameEngine.isGameFinished()) {
+                gameEngine.makeMove(board, moveId, messanger);
+                moveId++;
+            }
+            gameEngine.updatePoints();
+            gameEngine.displayCurrentResult(messanger);
+            gameId++;
+        }
+    }
+}
