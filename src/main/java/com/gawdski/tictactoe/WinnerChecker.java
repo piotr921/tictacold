b3b9ab1aa93697dca0736f416aad3a64336545af
Piotr Sekula <Piotr_Sekula@epam.com>
2017-07-04 09:34:17 +0200

    Refactor winner checker, to prepare this class for calculating more complex win cases

--- src/main/java/com/gawdski/tictactoe/WinnerChecker.java
+++ src/main/java/com/gawdski/tictactoe/WinnerChecker.java
@@ -3,12 +3,16 @@ package com.gawdski.tictactoe;
 class WinnerChecker {
 
     // TODO: for now board size is equal to winning condition
-    private final int boardSize;
+    private final int boardWidth;
+    private final int boardHeight;
+    private final int needToWin;
     private final int firstElementOnBoardId = 1;
     private Symbol winningSymbol;
 
     WinnerChecker(int size) {
-        this.boardSize = size;
+        this.boardHeight = size;
+        this.boardWidth = size;
+        this.needToWin = size;
     }
 
     GameState checkGameState(Board board) {
@@ -17,17 +21,56 @@ class WinnerChecker {
 
     private boolean gameWon(Board board) {
 
-        if (board.takenTiles() > boardSize) {
-            for (int firstFieldInRowId = firstElementOnBoardId; firstFieldInRowId <= boardSize * boardSize;
-                 firstFieldInRowId += boardSize) {
+        if (board.takenTiles() > needToWin) {
+            for (int firstFieldInRowId = firstElementOnBoardId; firstFieldInRowId <= boardWidth * boardHeight;
+                 firstFieldInRowId += boardWidth) {
                 if (winInRow(board, firstFieldInRowId)) return true;
             }
 
-            for (int firstFieldInColumnId = firstElementOnBoardId; firstFieldInColumnId <= boardSize; firstFieldInColumnId++) {
+            for (int firstFieldInColumnId = firstElementOnBoardId; firstFieldInColumnId <= boardWidth; firstFieldInColumnId++) {
                 if (winInColumn(board, firstFieldInColumnId)) return true;
             }
 
-            return winInDiagonal(board, firstElementOnBoardId) || winInDiagonal(board, boardSize);
+            return winInDiagonal(board, firstElementOnBoardId) || winInDiagonal(board, boardWidth);
+        }
+        return false;
+    }
+
+    private boolean winInRow(Board board, int firstFieldInRowId) {
+
+        winningSymbol = board.getTile(firstFieldInRowId);
+        if (!winningSymbol.equals(Symbol.EMPTY)) {
+            int counts = 1;
+            for (int fieldInRow = firstFieldInRowId + 1; fieldInRow < firstFieldInRowId + boardWidth; fieldInRow++) {
+                if (board.areFieldsEqual(fieldInRow, winningSymbol)) {
+                    counts++;
+                }
+            }
+            if (counts == needToWin) return true;
+        }
+        return false;
+/*        for (int fieldId = firstFieldInRowId; fieldId <= firstFieldInRowId + boardWidth; fieldId++) {
+            winningSymbol = board.getTile(firstFieldInRowId);
+            if (!winningSymbol.equals(Symbol.EMPTY)){
+                int counts = 1;
+
+            }
+        }*/
+    }
+
+    private boolean winInColumn(Board board, int firstFieldInColumnId) {
+
+        winningSymbol = board.getTile(firstFieldInColumnId);
+        // todo: change when not all fields in column will be necessary to win
+        if (!winningSymbol.equals(Symbol.EMPTY)) {
+            int counts = 1;
+            for (int fieldInColumn = firstFieldInColumnId + boardWidth; fieldInColumn <= boardWidth * boardHeight;
+                 fieldInColumn += boardWidth) {
+                if (board.areFieldsEqual(fieldInColumn, winningSymbol)) {
+                    counts++;
+                }
+            }
+            if (counts == needToWin) return true;
         }
         return false;
     }
@@ -37,48 +80,15 @@ class WinnerChecker {
         winningSymbol = board.getTile(startingField);
         if (!winningSymbol.equals(Symbol.EMPTY)) {
             int counts = 0;
-            for (int elementOnDiagonal = 0; elementOnDiagonal < boardSize; elementOnDiagonal++) {
+            for (int elementOnDiagonal = 0; elementOnDiagonal < boardWidth; elementOnDiagonal++) {
                 int currentElementId = getCalculateCurrentElementId(elementOnDiagonal, startingField);
-                if (board.areFieldsEqual(currentElementId, winningSymbol)){
+                if (board.areFieldsEqual(currentElementId, winningSymbol)) {
                     counts++;
                 }
-                if (counts == boardSize) {
+                if (counts == needToWin) {
                     return true;
                 }
             }
-        }
-        return false;
-    }
-
-    private boolean winInRow(Board board, int firstFieldInRowId) {
-
-        winningSymbol = board.getTile(firstFieldInRowId);
-        // todo: change when not all fields in row will be necessary to win
-        if (!winningSymbol.equals(Symbol.EMPTY)) {
-            int counts = 1;
-            for (int fieldInRow = firstFieldInRowId + 1; fieldInRow < firstFieldInRowId + boardSize; fieldInRow++) {
-                if (board.areFieldsEqual(fieldInRow, winningSymbol)) {
-                    counts++;
-                }
-            }
-            if (counts == boardSize) return true;
-        }
-        return false;
-    }
-
-    private boolean winInColumn(Board board, int firstFieldInColumnId) {
-
-        winningSymbol = board.getTile(firstFieldInColumnId);
-        // todo: change when not all fields in column will be necessary to win
-        if (!winningSymbol.equals(Symbol.EMPTY)) {
-            int counts = 1;
-            for (int fieldInColumn = firstFieldInColumnId + boardSize; fieldInColumn <= boardSize * boardSize;
-                 fieldInColumn += boardSize) {
-                if (board.areFieldsEqual(fieldInColumn, winningSymbol)) {
-                    counts++;
-                }
-            }
-            if (counts == boardSize) return true;
         }
         return false;
     }
@@ -90,11 +100,11 @@ class WinnerChecker {
     private int calculateDistanceBetweenFieldsOnDiagonal(int startingPoint) {
 
         if (startingPoint == firstElementOnBoardId) {
-            return boardSize + 1;
-        } else if (startingPoint == boardSize) {
-            return boardSize - 1;
+            return boardWidth + 1;
+        } else if (startingPoint == boardWidth) {
+            return boardWidth - 1;
         } else {
-            return boardSize;
+            return boardWidth;
         }
     }
 }
