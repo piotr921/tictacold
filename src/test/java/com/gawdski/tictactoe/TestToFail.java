f87a8e4ca4c82612f9f8140056e563d08edc8bd3
Piotr Sekula <piotr92345@o2.pl>
2017-07-20 15:30:59 +0200

    add failing test

---
+++ src/test/java/com/gawdski/tictactoe/TestToFail.java
@@ -0,0 +1,12 @@
+package com.gawdski.tictactoe;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestToFail {
+
+    @Test
+    public void failTest(){
+        Assert.assertTrue(false);
+    }
+}
